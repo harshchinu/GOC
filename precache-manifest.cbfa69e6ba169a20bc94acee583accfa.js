cef46d74f026d17cfbe3f4e9d5946165c13907f3
harshchinu <mharsh301@gmail.com>
2020-05-30 20:23:36 +0530

    Updates

---
+++ precache-manifest.cbfa69e6ba169a20bc94acee583accfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46635707a71d96048a3b8280558ef985",
+    "url": "/goc/index.html"
+  },
+  {
+    "revision": "ccf736d5c71ecf9260be",
+    "url": "/goc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "864a74e6226a90119fe5",
+    "url": "/goc/static/js/2.67916352.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goc/static/js/2.67916352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf736d5c71ecf9260be",
+    "url": "/goc/static/js/main.44ca86bf.chunk.js"
+  },
+  {
+    "revision": "9c204095324daa1df140",
+    "url": "/goc/static/js/runtime-main.0427f867.js"
+  }
+]);
