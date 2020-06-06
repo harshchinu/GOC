0ec6f8a736677c4f8580bc06913d9944e9802d49
harshchinu <mharsh301@gmail.com>
2020-06-06 19:30:27 +0530

    beta

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter,BrowserRouter, Route, Switch} from 'react-router-dom';
 import Welcome from './Components/Welcome';
 import Home from './Components/Home';
 
@@ -7,8 +7,8 @@ import Home from './Components/Home';
 const Routes = () => (
 <BrowserRouter >
     <Switch>
-    <Route exact path="/" component={Welcome}/>
-    <Route path="/home" component={Home}/>  
+    <Route exact path={process.env.PUBLIC_URL +"/"} component={Welcome}/>
+    <Route path={process.env.PUBLIC_URL +"/home"} component={Home}/>  
    </Switch>
 </BrowserRouter>
 );
